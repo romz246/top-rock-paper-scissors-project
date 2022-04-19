a6322f326cfe45bbdd527b90f1ba234d50114963
Romario <romarioclarke5@gmail.com>
2022-04-19 11:28:02 -0400

    Add playRound function

--- js/main-script.js
+++ js/main-script.js
@@ -19,3 +19,12 @@ function playerPlay() {
 function confirmChoice (choice) {
   return choices.includes(choice);
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerPlay();
+  computerSelection = computerPlay();
+  const winner = checkWinner(playerSelection, computerSelection);
+  console.log("Player picked: " + playerSelection);
+  console.log("Computer picked: " + computerSelection);
+  console.log(winner);
+}
