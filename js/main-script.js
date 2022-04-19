bed1fe8f143da8cdd6f94ac27423876cdfd5f820
Romario <romarioclarke5@gmail.com>
2022-04-19 11:09:09 -0400

    Add playerPlay function

--- js/main-script.js
+++ js/main-script.js
@@ -3,3 +3,16 @@ const choices = ['rock', 'paper', 'scissors'];
 function computerPlay() {
   return choices [Math.floor(Math.random() * choices.length)];
 }
+
+
+function playerPlay() {
+  let playerSelection = prompt("Please type 'rock', 'paper' or 'scissors'.");
+  playerSelection = playerSelection.toLowerCase();
+  let check = confirmChoice(playerSelection);
+  while (check === false) {
+    playerSelection = prompt("Invalid input! Please type 'rock', 'paper' or 'scissors'.");
+    playerSelection.toLowerCase();
+    check = confirmChoice(playerSelection);
+  }
+  return playerSelection;
+}
